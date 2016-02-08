9ab4ae4c942323aca190b5d93af66734aca8b771
ajlopez <webmaster@ajlopez.com>
2016-02-08 12:50:48 -0300

    string source

--- src/crlisp.cr
+++ src/crlisp.cr
@@ -1,4 +1,4 @@
 
 require "./crlisp/token.cr"
 require "./crlisp/lexer.cr"
-
+require "./crlisp/string_source.cr"
---
+++ src/crlisp/string_source.cr
@@ -0,0 +1,28 @@
+
+module CrLisp
+
+	class StringSource	
+		def initialize(text)
+			@text = text
+			@position = 0
+			@chars = []
+		end
+		
+		def nextChar
+			char = @chars.pop
+			
+			if char
+				return char
+			end
+		
+			char = @text[@position]
+			@position += 1
+			return char
+		end
+		
+		def pushChar(ch)
+			@chars.push ch
+		end
+	end
+
+end
