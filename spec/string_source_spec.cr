649574bd2688d6b20ecc15d8c4d774d8d267648e
ajlopez <webmaster@ajlopez.com>
2016-02-09 10:45:26 -0300

    new spec test for string source

--- spec/string_source_spec.cr
+++ spec/string_source_spec.cr
@@ -11,6 +11,23 @@ module CrLisp
                 char.should_not be_nil
                 char.should eq('a')
             end
+
+            it "get two chars" do
+                source = StringSource.new "ab"
+                
+                char = source.nextChar
+                
+                char.should_not be_nil
+                char.should eq('a')
+                
+                char = source.nextChar
+                
+                char.should_not be_nil
+                char.should eq('b')
+                
+                char = source.nextChar
+                char.should be_nil
+            end
         end
     end
 end
